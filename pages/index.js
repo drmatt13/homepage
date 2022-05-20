159d326079319f34db0ada18670ab4ee1fbc8d9a
Matthew Sweeney <Matthew.Sweeney001@gmail.com>
2022-05-20 00:57:08 -0400

    update 3

--- pages/index.js
+++ pages/index.js
@@ -19,21 +19,21 @@ export default function Home() {
 
   const [items] = useState([
     {
-      title: "School",
+      title: "coding",
       list: [
-        ["mySNHU", "https://mysnhu.force.com/mysnhu/s/"],
-        ["SNHU Webmail", "https://sso-mail.snhu.edu/owa"],
-        ["DAT-375", "https://learn.snhu.edu/d2l/home/1071736"],
-        ["IT-226", "https://learn.snhu.edu/d2l/home/1067966"],
+        ["Vercel", "https://vercel.com/dashboard"],
+        ["Github", "https://github.com/"],
+        ["Box Shadows", "https://getcssscan.com/css-box-shadow-examples"],
+        ["uiGradients", "https://uigradients.com"],
       ],
     },
     {
-      title: "Tools",
+      title: "School",
       list: [
-        ["Google Docs", "https://docs.google.com/"],
-        ["Box Shadows", "https://getcssscan.com/css-box-shadow-examples"],
-        ["Diagram Maker", "https://app.diagrams.net/"],
-        ["uiGradients", "https://uigradients.com"],
+        ["Udemy", "https://www.udemy.com/"],
+        ["mySNHU", "https://mysnhu.force.com/mysnhu/s/"],
+        ["DAT-375", "https://learn.snhu.edu/d2l/home/1071736"],
+        ["IT-226", "https://learn.snhu.edu/d2l/home/1067966"],
       ],
     },
     {
@@ -59,11 +59,36 @@ export default function Home() {
       ],
     },
     {
+      title: "Tools",
+      list: [
+        ["Google Docs", "https://docs.google.com/"],
+        ["Clip Path", "https://bennettfeely.com/clippy"],
+        ["GIF Maker", "https://ezgif.com/maker"],
+        ["Diagram Maker", "https://app.diagrams.net/"],
+      ],
+    },
+    {
       title: "Social",
       list: [
         ["Banned Social", "https://banned-social2.vercel.app/"],
         ["Instagram", "https://instagram.com/"],
         ["Facebook", "https://facebook.com/"],
+      ],
+    },
+    {
+      title: "Job Search",
+      list: [
+        ["HandShake", "https://joinhandshake.com/"],
+        ["LinkedIn", "https://www.linkedin.com/"],
+        ["Upwork", "https://www.upwork.com/"],
+      ],
+    },
+    {
+      title: "Video",
+      list: [
+        ["Banned Video", "https://banned.video"],
+        ["Bitchute", "https://www.bitchute.com/"],
+        ["youtube", "https://youtube.com"],
       ],
     },
     {
@@ -101,7 +126,7 @@ export default function Home() {
         <div className="w-full flex justify-center pt-8">
           <form onSubmit={handleSubmit}>
             <input
-              className="px-4 py-1 rounded-full"
+              className="px-4 py-1 rounded-full w-72 shadow-lg max-w-[80vw]"
               ref={searchRef}
               type="text"
               value={search}
@@ -114,7 +139,7 @@ export default function Home() {
           {items.map((item, i) => (
             <>
               <div key={i}>
-                <div className="flex flex-col h-full bg-white/70 backdrop-blur-lg p-3 rounded-md">
+                <div className="flex flex-col h-full bg-white/70 p-3 rounded-md shadow-lg">
                   <div className="text-center mb-2 text-lg font-bold">
                     {item.title}
                   </div>
